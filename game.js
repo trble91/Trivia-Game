31879cd0938038cc88e6423fdfbca1f9e4c5b1e0
trble91 <daleyaaron@gmail.com>
2023-03-24 10:05:46 -0400

    STarter code

---
+++ game.js
@@ -0,0 +1,25 @@
+const Questions = [{
+        id: 0,
+        q: 'What is the birthplace of Hip-Hop',
+        a: [{'Bronx, NY'},
+            {'Detroit, MI'},
+            {'Atlanta, GA'},
+            {'Silver Springs, MD'},],
+    id: 1,
+        q: 'Who is regarded as the Godfather of Hip-Hop',
+        a: [{'DJ Mustard'},
+            {'DJ Kool Herc'},
+            {'DJ AM'},
+            {'DJ Jazzy Jeff & The Fresh Prince'},],
+    id: 2,
+        
+    id: 3,
+    q: "What is One of the Five elements of Hip-Hop?",
+    a: [{ text: "gandhinagar", isCorrect: false },
+        { text: "Surat", isCorrect: false },
+        { text: "Delhi", isCorrect: true },
+        { text: "mumbai", isCorrect: false }
+    ]
+},
+
+console.log(Questions[0])
